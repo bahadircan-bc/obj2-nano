8727a60f0343754c7b3397a0c81fab7e4d935bc8
Bahadır Can <bahadircan.bc@gmail.com>
2024-01-31 16:04:59 +0300

    added core and highgui includes explicitely

--- readMultipleCam.h
+++ readMultipleCam.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <map>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <unistd.h>
 #include <thread>
 #include <atomic>
